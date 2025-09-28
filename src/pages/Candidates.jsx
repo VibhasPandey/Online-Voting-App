e766776531011e31a8b26fa70dbcdea2832d6f8e
Vibhas <pandeyvibhas7@gmail.com>
2025-09-29 00:00:21 +0530

    created candidates pages

--- src/pages/Candidates.jsx
+++ src/pages/Candidates.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
-
+import {candidates as dummyCandidates} from '../data'
+import { useParams } from 'react-router-dom'
+import Candidate from '../components/Candidate'
 const Candidates = () => {
+
+  const {id} = useParams()
+  const candidates=dummyCandidates.filter(candidate=>candidate.election===id)
+
+
   return (
-    <div>Candidates</div>
+    <section className="candidates">
+      <header className="candidates_header">
+        <h1>
+          VOTE YOUR CANDIDATE
+        </h1>
+        <p>
+          These are the candidates for the selected election. Please vote once and wisely, because you won't be allowed to vote in this election again
+        </p>
+      </header>
+      <div className="container candidates_container">
+            { 
+              candidates.map(candidate=><Candidate key={candidate.id} {...candidate} />)
+            }
+      </div>
+    </section>
   )
 }
 
